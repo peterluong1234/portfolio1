3a25e751809cc62d7c91f108235de5cc75aa511c
Peter <peterluong1123@gmail.com>
2023-05-09 12:32:23 -0700

    fix apostrophe in /description component

--- components/Description.js
+++ components/Description.js
@@ -1,10 +1,11 @@
 import styles from "../styles/Description.module.scss"
 
 export const Description = ({description}) => {
+    const experienceIntro = 'Here are some of the brands that I\'ve worked with';
     return(
         <div className={styles.container}>
             <div className={styles.description}>{description}</div>
-            <div className={styles.experience}>Here are some of the brands that I've worked with</div>
+            <div className={styles.experience}>{experienceIntro}</div>
         </div>
     )
 }
