17573d43a6ecec06e1e963982e7b971a10a3d976
Peter Luong <peterluong1123@gmail.com>
2024-08-11 17:03:58 -0700

    updated data

--- public/data.js
+++ public/data.js
@@ -6,7 +6,7 @@ export const data = {
         experienceIntro: 'Here are some of the brands and organizations that I\'ve worked with',
     },
     aboutMe: {
-        header: 'Graphic Designer turned Full Stack Developer with a passion for the outdoors',
+        header: 'Software Engineer with a passion for the outdoors',
         paragraph: [
             'Peter loves the outdoors. Raised as a kid of the streets, he spent most of his youth riding his bike with neighborhood kids, skateboarding, and playing basketball.',
             ' This eventually turned into a passion for an active lifestyle. His current hobbies include snowboarding, bouldering, cycling, mountain biking, gymming, playing piano and singing.',
