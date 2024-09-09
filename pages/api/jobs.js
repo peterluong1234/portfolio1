8115a9ac9378c4b5534025dbdcc6823c84b2c47a
Peter <peterluong1123@gmail.com>
2024-09-09 00:21:22 -0700

    prep api for other http requests

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -4,9 +4,16 @@ export default async function handler(req, res) {
   try {
     const client = await clientPromise
     const db = client.db('workExperienceDb')
-    const jobs = await db.collection('work').find({}).toArray();
 
-    res.status(200).json(jobs);
+    switch (req.method) {
+      case 'GET':
+        const jobs = await db.collection('work').find({}).toArray();
+        res.status(200).json(jobs);
+        break;
+      default:
+        res.setHeader('Allow', ['GET']);
+        res.status(405).json({ message: `Method ${req.method} not allowed` });
+    }
   } catch (e) {
     console.error(e)
     res.status(500).json({ message: 'Internal Server Error'});
