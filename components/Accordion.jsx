3d54c2fcfc935b4764202cf5499f7d227f66d1d6
peterluong1234 <peterluong1123@gmail.com>
2024-09-25 16:51:06 -0700

    create accordion component

---
+++ components/Accordion.jsx
@@ -0,0 +1,68 @@
+import styles from "../styles/Work.module.scss";
+import { useEffect, useState } from "react";
+
+export const Accordion = ({ work }) => {
+  const [expandedWork, setExpandedWork] = useState({});
+
+  const toggleWorkItem = (index) => {
+    setExpandedWork((prevExpanded) => ({
+      ...prevExpanded,
+      [index]: !prevExpanded[index],
+    }));
+  };
+  return (
+    <>
+      {work.map((workplace, idx) => (
+        <div
+          key={idx}
+          className={styles.workContainer}
+          onClick={() => toggleWorkItem(idx)}
+        >
+          <div className={styles.workHeaderContainer}>
+            <div className={styles.workTextContainer}>
+              <h2>{workplace.companyName}</h2>
+              <p>{workplace.role}</p>
+              <small>{workplace.period}</small>
+            </div>
+            <img
+              src="https://cdn.prod.website-files.com/66476c64e241589e9d182b69/668ccdcf8c39cf84da6bd553_plus.svg"
+              alt=""
+            />
+          </div>
+          <div
+            className={styles.workContentContainer}
+            style={{
+              height: expandedWork[idx] ? "fit-content" : "0px",
+            }}
+          >
+            <div className={styles.workContent}>
+              <hr></hr>
+              <p>{workplace.description}</p>
+              {workplace.tasks ? <h3>Tasks</h3> : null}
+              {workplace.tasks
+                ? workplace.tasks.map((task, i) => (
+                    <div key={i}>
+                      <p>{task}</p>
+                    </div>
+                  ))
+                : null}
+              {workplace.projects ? <h3>Projects</h3> : null}
+              {workplace.projects
+                ? workplace.projects.map((project, i) => (
+                    <div key={i}>
+                      <p>{project.description}</p>
+                      {project.keyPoints
+                        ? project.keyPoints.map((keyPoint, kdx) => (
+                            <div key={kdx}>{keyPoint}</div>
+                          ))
+                        : null}
+                    </div>
+                  ))
+                : null}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "../../components/Navbar"
 import Head from 'next/head';
 import styles from '../../styles/Work.module.scss'
 import { useEffect, useState } from "react";
+import { Accordion } from "../../components/Accordion";
 // const WORK = [
 //     {
 //         companyName: "Zymo Research",
@@ -121,7 +122,6 @@ import { useEffect, useState } from "react";
 
 
 const Work = ({image,brand}) => {
-    const [expandedWork, setExpandedWork] = useState({});
     const [work, setWork] = useState([]);
 
     useEffect(() => {
@@ -143,14 +143,6 @@ const Work = ({image,brand}) => {
         fetchJobs();
     }, [])
 
-    const toggleWorkItem = (index) => {
-        setExpandedWork((prevExpanded) => ({
-            ...prevExpanded,
-            [index]: !prevExpanded[index]
-        }))
-    }
-
-
     return (
     <div>
         <Head>
@@ -162,60 +154,8 @@ const Work = ({image,brand}) => {
         <main className={styles.pageContainer}>
             <div className={styles.headerContainer}><h1>Work</h1></div>
             <section>
-                <div>
-                    {
-                        work.map((workplace, idx) => (
-                        <div
-                            key={idx}
-                            className={ styles.workContainer }
-                            onClick={() => toggleWorkItem(idx)}
-                        >
-                            <div className={ styles.workHeaderContainer }>
-                                <div className={styles.workTextContainer}>
-                                    <h2>{workplace.companyName}</h2>
-                                    <p>{workplace.role}</p>
-                                    <small>{workplace.period}</small>
-                                </div>
-                                <img src="https://cdn.prod.website-files.com/66476c64e241589e9d182b69/668ccdcf8c39cf84da6bd553_plus.svg" alt="" />
-                            </div>
-                            <div
-                                className={ styles.workContentContainer}
-                                style={{
-                                    height: expandedWork[idx] ? 'fit-content' : '0px'
-                                }}
-                            >
-                                <div className={styles.workContent}>
-                                <hr></hr>
-                                <p>{workplace.description}</p>
-                                { workplace.tasks ? <h3>Tasks</h3> : null }
-                                    { workplace.tasks ? workplace.tasks.map((task,i) => (
-                                    <div key={i}>
-                                        <p>{task}</p>
-                                    </div>
-                                        ))
-                                        : null
-                                    }
-                                { workplace.projects ? <h3>Projects</h3> : null }
-                                    { workplace.projects ? workplace.projects.map((project,i) => (
-                                    <div key={i}>
-                                        <p>{project.description}</p>
-                                        { project.keyPoints ? project.keyPoints.map((keyPoint, kdx) => (
-                                            <div key={kdx}>{keyPoint}</div>
-                                        )) : null
-                                    }
-                                    </div>
-                                        ))
-                                        : null
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                        ))
-                    }
-                    
-                </div>
+                <Accordion work={work} />
             </section>
-
         </main>
     </div>
     )
